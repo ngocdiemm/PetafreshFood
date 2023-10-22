6dab248921efc29a009fb70fb7d21d8ea6eb7163
ngocdiemm <ngocdiem.le.2803@gmail.com>
2023-10-22 07:22:56 +0700

    UI

---
+++ Petafresh-Food/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Lấy tham chiếu tới nút "Xem sản phẩm" bằng id
+    const viewProductsButton = document.getElementById("viewProductsButton");
+
+    // Xử lý sự kiện khi nút được nhấn
+    viewProductsButton.addEventListener("click", function () {
+        // Chuyển hướng đến trang "products.html"
+        window.location.href = "products.html";
+    });
+});
